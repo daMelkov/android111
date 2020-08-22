89e6baea2f0ee0952fa06e4bd1570293cfb72675
Melkov Denis <d.a.melkov@gmail.com>
2020-08-22 06:05:38 +0300

    add support-class Data for static init

--- app/src/main/java/com/astra/melkovhw111/MainActivity.java
+++ app/src/main/java/com/astra/melkovhw111/MainActivity.java
@@ -16,19 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task (
-                "MelkovHw51",
-                "Записная книжка в SharedPreferences",
-                "Одно из самых популярных приложений на телефон - это заметки. Давайте создадим свои собственные. Элементы приложения:\n" +
-                        "\n" +
-                        "- EditText с заметкой.\n" +
-                        "- Кнопка “Сохранить” (при клике на нее, заметка сохраняется в SharedPreferences). При перезапуске приложения, если заметка ранее была сохранена, она отображается в EditText компоненте.",
-                R.drawable.hw51)
-        );
+        List<Task> tasks = Data.initTasks();
 
         BaseAdapter adapter = new TaskAdapter(MainActivity.this, tasks);
-
         ListView listView = findViewById(R.id.list_view);
         listView.setAdapter(adapter);
     }
